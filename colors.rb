ec852dbe8acead6ccfd4b66fd238d2f729f77dcd
Beki <bereketadego@gmail.com>
2022-04-13 15:25:27 +0300

    add more colors for terminal

--- colors.rb
+++ colors.rb
@@ -3,4 +3,31 @@ class String
     "\e[30m#{self}\e[0m"
   end
 
- 
+  def red
+    "\e[31m#{self}\e[0m"
+  end
+
+  def green
+    "\e[32m#{self}\e[0m"
+  end
+
+  def yellow
+    "\e[33m#{self}\e[0m"
+  end
+
+  def brown
+    "\e[33m#{self}\e[0m"
+  end
+
+  def blue
+    "\e[34m#{self}\e[0m"
+  end
+
+  def white
+    "\e[37m#{self}\e[0m"
+  end
+
+  def bold
+    "\e[1m#{self}\e[22m"
+  end
+end
